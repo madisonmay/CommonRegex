e7223ad5c12da24a9a7345d70d78f147abe37414
Madison May <madison.may@students.olin.edu>
2014-01-21 18:14:16 -0500

    Manually add list of tlds in order to support subdomains and urls without http:// or www

--- commonregex.py
+++ commonregex.py
@@ -4,7 +4,7 @@ import re
 date        = re.compile(u'(?:(?<!\:)(?<!\:\d)[0-3]?\d(?:st|nd|rd|th)?\s+(?:of\s+)?(?:jan\.?|january|feb\.?|february|mar\.?|march|apr\.?|april|may|jun\.?|june|jul\.?|july|aug\.?|august|sep\.?|september|oct\.?|october|nov\.?|november|dec\.?|december)|(?:jan\.?|january|feb\.?|february|mar\.?|march|apr\.?|april|may|jun\.?|june|jul\.?|july|aug\.?|august|sep\.?|september|oct\.?|october|nov\.?|november|dec\.?|december)\s+(?<!\:)(?<!\:\d)[0-3]?\d(?:st|nd|rd|th)?)(?:\,)?\s*(?:\d{4})?|[0-3]?\d[-\./][0-3]?\d[-\./]\d{2,4}', re.IGNORECASE)
 time        = re.compile(u'\d{1,2}:\d{2} ?(?:[ap]\.?m\.?)?|\d[ap]\.?m\.?', re.IGNORECASE)
 phone       = re.compile(u'((?:(?<![\d-])(?:\+?\d{1,3}[-.\s*]?)?(?:\(?\d{3}\)?[-.\s*]?)?\d{3}[-.\s*]?\d{4}(?![\d-]))|(?:(?<![\d-])(?:(?:\(\+?\d{2}\))|(?:\+?\d{2}))\s*\d{2}\s*\d{3}\s*\d{4}(?![\d-])))')
-link        = re.compile(u'(?i)((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+[^\s`!()\[\]{};:\'".,<>?\xab\xbb\u201c\u201d\u2018\u2019]))', re.IGNORECASE)
+link        = re.compile(u'(?i)((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.](?:(?:ac)|(?:academy)|(?:ad)|(?:ae)|(?:aero)|(?:af)|(?:ag)|(?:agency)|(?:ai)|(?:al)|(?:am)|(?:an)|(?:ao)|(?:aq)|(?:ar)|(?:arpa)|(?:as)|(?:asia)|(?:at)|(?:au)|(?:aw)|(?:ax)|(?:az)|(?:ba)|(?:bb)|(?:bd)|(?:be)|(?:berlin)|(?:bf)|(?:bg)|(?:bh)|(?:bi)|(?:bike)|(?:biz)|(?:bj)|(?:bm)|(?:bn)|(?:bo)|(?:br)|(?:bs)|(?:bt)|(?:build)|(?:builders)|(?:buzz)|(?:bv)|(?:bw)|(?:by)|(?:bz)|(?:ca)|(?:cab)|(?:camera)|(?:camp)|(?:careers)|(?:cat)|(?:cc)|(?:cd)|(?:center)|(?:ceo)|(?:cf)|(?:cg)|(?:ch)|(?:cheap)|(?:ci)|(?:ck)|(?:cl)|(?:clothing)|(?:club)|(?:cm)|(?:cn)|(?:co)|(?:codes)|(?:coffee)|(?:com)|(?:company)|(?:computer)|(?:construction)|(?:contractors)|(?:coop)|(?:cr)|(?:cu)|(?:cv)|(?:cw)|(?:cx)|(?:cy)|(?:cz)|(?:dance)|(?:de)|(?:democrat)|(?:diamonds)|(?:directory)|(?:dj)|(?:dk)|(?:dm)|(?:do)|(?:domains)|(?:dz)|(?:ec)|(?:edu)|(?:education)|(?:ee)|(?:eg)|(?:email)|(?:enterprises)|(?:equipment)|(?:er)|(?:es)|(?:estate)|(?:et)|(?:eu)|(?:farm)|(?:fi)|(?:fj)|(?:fk)|(?:florist)|(?:fm)|(?:fo)|(?:fr)|(?:ga)|(?:gallery)|(?:gb)|(?:gd)|(?:ge)|(?:gf)|(?:gg)|(?:gh)|(?:gi)|(?:gift)|(?:gl)|(?:glass)|(?:gm)|(?:gn)|(?:gov)|(?:gp)|(?:gq)|(?:gr)|(?:graphics)|(?:gs)|(?:gt)|(?:gu)|(?:guitars)|(?:guru)|(?:gw)|(?:gy)|(?:hk)|(?:hm)|(?:hn)|(?:holdings)|(?:holiday)|(?:house)|(?:hr)|(?:ht)|(?:hu)|(?:id)|(?:ie)|(?:il)|(?:im)|(?:immobilien)|(?:in)|(?:info)|(?:institute)|(?:int)|(?:international)|(?:io)|(?:iq)|(?:ir)|(?:is)|(?:it)|(?:je)|(?:jm)|(?:jo)|(?:jobs)|(?:jp)|(?:kaufen)|(?:ke)|(?:kg)|(?:kh)|(?:ki)|(?:kitchen)|(?:kiwi)|(?:km)|(?:kn)|(?:kp)|(?:kr)|(?:kw)|(?:ky)|(?:kz)|(?:la)|(?:land)|(?:lb)|(?:lc)|(?:li)|(?:lighting)|(?:limo)|(?:link)|(?:lk)|(?:lr)|(?:ls)|(?:lt)|(?:lu)|(?:luxury)|(?:lv)|(?:ly)|(?:ma)|(?:management)|(?:marketing)|(?:mc)|(?:md)|(?:me)|(?:menu)|(?:mg)|(?:mh)|(?:mil)|(?:mk)|(?:ml)|(?:mm)|(?:mn)|(?:mo)|(?:mobi)|(?:moda)|(?:monash)|(?:mp)|(?:mq)|(?:mr)|(?:ms)|(?:mt)|(?:mu)|(?:museum)|(?:mv)|(?:mw)|(?:mx)|(?:my)|(?:mz)|(?:na)|(?:name)|(?:nc)|(?:ne)|(?:net)|(?:nf)|(?:ng)|(?:ni)|(?:ninja)|(?:nl)|(?:no)|(?:np)|(?:nr)|(?:nu)|(?:nz)|(?:om)|(?:onl)|(?:org)|(?:pa)|(?:pe)|(?:pf)|(?:pg)|(?:ph)|(?:photo)|(?:photography)|(?:photos)|(?:pics)|(?:pink)|(?:pk)|(?:pl)|(?:plumbing)|(?:pm)|(?:pn)|(?:post)|(?:pr)|(?:pro)|(?:ps)|(?:pt)|(?:pw)|(?:py)|(?:qa)|(?:re)|(?:recipes)|(?:red)|(?:repair)|(?:rich)|(?:ro)|(?:rs)|(?:ru)|(?:ruhr)|(?:rw)|(?:sa)|(?:sb)|(?:sc)|(?:sd)|(?:se)|(?:sexy)|(?:sg)|(?:sh)|(?:shiksha)|(?:shoes)|(?:si)|(?:singles)|(?:sj)|(?:sk)|(?:sl)|(?:sm)|(?:sn)|(?:so)|(?:social)|(?:solar)|(?:solutions)|(?:sr)|(?:st)|(?:su)|(?:support)|(?:sv)|(?:sx)|(?:sy)|(?:systems)|(?:sz)|(?:tattoo)|(?:tc)|(?:td)|(?:technology)|(?:tel)|(?:tf)|(?:tg)|(?:th)|(?:tips)|(?:tj)|(?:tk)|(?:tl)|(?:tm)|(?:tn)|(?:to)|(?:today)|(?:tp)|(?:tr)|(?:training)|(?:travel)|(?:tt)|(?:tv)|(?:tw)|(?:tz)|(?:ua)|(?:ug)|(?:uk)|(?:uno)|(?:us)|(?:uy)|(?:uz)|(?:va)|(?:vc)|(?:ve)|(?:ventures)|(?:vg)|(?:vi)|(?:viajes)|(?:vn)|(?:voyage)|(?:vu)|(?:wang)|(?:wf)|(?:wien)|(?:ws))/)(?:[^\s()<>]+[^\s`!()\[\]{};:\'".,<>?\xab\xbb\u201c\u201d\u2018\u2019]))', re.IGNORECASE)
 email       = re.compile(u"([a-z0-9!#$%&'*+\/=?^_`{|.}~-]+@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?)", re.IGNORECASE)
 ip          = re.compile(u'(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)', re.IGNORECASE)
 ipv6        = re.compile(u'\s*(?!.*::.*::)(?:(?!:)|:(?=:))(?:[0-9a-f]{0,4}(?:(?<=::)|(?<!::):)){6}(?:[0-9a-f]{0,4}(?:(?<=::)|(?<!::):)[0-9a-f]{0,4}(?:(?<=::)|(?<!:)|(?<=:)(?<!::):)|(?:25[0-4]|2[0-4]\d|1\d\d|[1-9]?\d)(?:\.(?:25[0-4]|2[0-4]\d|1\d\d|[1-9]?\d)){3})\s*', re.VERBOSE|re.IGNORECASE|re.DOTALL)
@@ -46,21 +46,3 @@ class CommonRegex(object):
             for key in regexes.keys():
                 method = getattr(self, key)
                 setattr(self, key, method())
-
-if __name__ == "__main__":
-    test = """"8:00 5:00AM Jan 9th 2012 8/23/12 www.google.com $4891.75
-               2001:0db8::ff00:0042:8329 http://hotmail.com (520) 820 7123,
-               1-230-241-2422 john_smith@gmail.com 127.0.0.1 #e9be4fff 1234567891011121
-               1234-5678-9101-1121 """
-
-    parse = CommonRegex(test)
-    assert(parse.dates        == ['Jan 9th 2012', '8/23/12'])
-    assert(parse.times        == ['8:00', '5:00AM', '00:00', '42:83'])
-    assert(parse.phones       == ['(520) 820 7123', '1-230-241-2422'])
-    assert(parse.links        == ['www.google.com', 'http://hotmail.com'])
-    assert(parse.emails       == ['john_smith@gmail.com'])
-    assert(parse.ips          == ['127.0.0.1'])
-    assert(parse.ipv6s        == ['2001:0db8::ff00:0042:8329'])
-    assert(parse.prices       == ['$4891.75'])
-    assert(parse.hex_colors   == ['#e9be4fff'])
-    assert(parse.credit_cards == ['1234567891011121', '1234-5678-9101-1121'])
--- test.py
+++ test.py
@@ -7,14 +7,14 @@ class TestDates(unittest.TestCase):
         self.parser = CommonRegex()
 
     def test_numeric(self):
-        formats = ["1-19-14", "1.19.14", "1.19.14", "01.19.14"]
-        for f in formats:
-            self.assertEqual(self.parser.dates(f), [f])
+        matching = ["1-19-14", "1.19.14", "1.19.14", "01.19.14"]
+        for s in matching:
+            self.assertEqual(self.parser.dates(s), [s])
 
     def test_verbose(self):
-        formats = ["January 19th, 2014", "Jan. 19th, 2014", "Jan 19 2014", "19 Jan 2014"]
-        for f in formats:
-            self.assertEqual(self.parser.dates(f), [f])
+        matching = ["January 19th, 2014", "Jan. 19th, 2014", "Jan 19 2014", "19 Jan 2014"]
+        for s in matching:
+            self.assertEqual(self.parser.dates(s), [s])
 
 class TestTimes(unittest.TestCase):
 
@@ -22,9 +22,9 @@ class TestTimes(unittest.TestCase):
         self.parser = CommonRegex()
 
     def test_times(self):
-        formats = ["09:45", "9:45", "23:45", "9:00am", "9am", "9:00 A.M.", "9:00 pm"]
-        for f in formats:
-            self.assertEqual(self.parser.times(f), [f])
+        matching = ["09:45", "9:45", "23:45", "9:00am", "9am", "9:00 A.M.", "9:00 pm"]
+        for s in matching:
+            self.assertEqual(self.parser.times(s), [s])
 
 class TestPhones(unittest.TestCase):
 
@@ -32,12 +32,12 @@ class TestPhones(unittest.TestCase):
         self.parser = CommonRegex()
 
     def test_phones(self):
-        formats = ["12345678900", "1234567890", "+1 234 567 8900", "234-567-8900",
+        matching = ["12345678900", "1234567890", "+1 234 567 8900", "234-567-8900",
                    "1-234-567-8900", "1.234.567.8900", "5678900", "567-8900", 
                    "(123) 456 7890", "+41 22 730 5989", "(+41) 22 730 5989",
                    "+442345678900"]
-        for f in formats:
-            self.assertEqual(self.parser.phones(f), [f])
+        for s in matching:
+            self.assertEqual(self.parser.phones(s), [s])
 
 class TestLinks(unittest.TestCase):
 
@@ -45,10 +45,14 @@ class TestLinks(unittest.TestCase):
         self.parser = CommonRegex()
 
     def test_links(self):
-        formats = ["www.google.com", "http://www.google.com", "www.google.com/?query=dog"
-                   "sub.example.com", "http://www.google.com/%&#/?q=dog"]
-        for f in formats:
-            self.assertEqual(self.parser.links(f), [f])
+        matching = ["www.google.com", "http://www.google.com", "www.google.com/?query=dog"
+                   "sub.example.com", "http://www.google.com/%&#/?q=dog", "google.com"]
+        non_matching = ["www.google.con"]
+        for s in matching:
+            self.assertEqual(self.parser.links(s), [s])
+        for s in non_matching:
+            self.assertNonEqual(self.parser.links(s), [s])
+
 
 if __name__ == '__main__':
     test_cases = [TestDates, TestTimes, TestPhones, TestTimes]
