bf70b412041b7759df4043d582def4c32b87ddc3
Cameron Dart <camerond@fair.com>
2018-07-26 11:06:38 -0700

    finished migrating to pytest

--- test.py
+++ test.py
@@ -73,126 +73,85 @@ def test_phones_with_extensions(phone_number):
     assert parser().phones_with_exts(phone_number) == [phone_number]
 
 
-@pytest.mark.parametrize('fake_number', [
-    "222-5555",
-    "222-8888 x 623",
-    "333-333-5555 dial 3"
+@pytest.mark.parametrize('link', [
+    "www.google.com",
+    "http://www.google.com",
+    "www.google.com/?query=dog"
+    "sub.example.com",
+    "http://www.google.com/%&#/?q=dog",
+    "google.com"
 ])
-def test_non_matching_fake_number(fake_number):
-    assert parser().links(fake_number) != [fake_number]
+def test_links(link):
+    assert parser().links(link) == [link]
 
-# class TestLinks(RegexTestCase):
-#
-#     def test_links(self):
-#         matching = ["www.google.com", "http://www.google.com", "www.google.com/?query=dog"
-#                    "sub.example.com", "http://www.google.com/%&#/?q=dog", "google.com"]
-#         non_matching = ["www.google.con"]
-#         for s in matching:
-#             self.assertEqual(self.parser.links(s), [s])
-#         for s in non_matching:
-#             self.assertNotEqual(self.parser.links(s), [s])
-#
-#
-# class TestEmails(RegexTestCase):
-#
-#     def test_emails(self):
-#         matching = ["john.smith@gmail.com", "john_smith@gmail.com", "john@example.net"]
-#         non_matching = ["john.smith@gmail..com"]
-#         for s in matching:
-#             self.assertEqual(self.parser.emails(s), [s])
-#         for s in non_matching:
-#             self.assertNotEqual(self.parser.emails(s), [s])
-#
-#
-# class TestIPs(RegexTestCase):
-#
-#     def test_ips(self):
-#         matching = ["127.0.0.1", "192.168.1.1", "8.8.8.8"]
-#         for s in matching:
-#             self.assertEqual(self.parser.ips(s), [s])
-#
-#
-# class TestIPv6s(RegexTestCase):
-#
-#     def test_ipv6s(self):
-#         matching = ["fe80:0000:0000:0000:0204:61ff:fe9d:f156", "fe80:0:0:0:204:61ff:fe9d:f156",
-#                     "fe80::204:61ff:fe9d:f156", "fe80:0000:0000:0000:0204:61ff:254.157.241.86",
-#                     "fe80:0:0:0:0204:61ff:254.157.241.86", "fe80::204:61ff:254.157.241.86", "::1"]
-#         for s in matching:
-#             self.assertEqual(self.parser.ipv6s(s), [s])
-#
-#
-# class TestPrices(RegexTestCase):
-#
-#     def test_prices(self):
-#         matching = ["$1.23", "$1", "$1,000", "$10,000.00"]
-#         non_matching = ["$1,10,0", "$100.000"]
-#         for s in matching:
-#             self.assertEqual(self.parser.prices(s), [s])
-#         for s in non_matching:
-#             self.assertNotEqual(self.parser.prices(s), [s])
-#
-#
-# class TestHexColors(RegexTestCase):
-#
-#     def test_hexcolors(self):
-#         matching = ["#fff", "#123", "#4e32ff", "#12345678"]
-#         for s in matching:
-#             self.assertEqual(self.parser.hex_colors(s), [s])
-#
-#
-# class TestCreditCards(RegexTestCase):
-#
-#     def test_creditcards(self):
-#         matching = ["0000-0000-0000-0000", "0123456789012345",
-#                     "0000 0000 0000 0000", "012345678901234"]
-#         for s in matching:
-#             self.assertEqual(self.parser.credit_cards(s), [s])
-#
-# class TestBTCAddresses(RegexTestCase):
-#
-#     def test_btc_addresses(self):
-#         matching = ["1LgqButDNV2rVHe9DATt6WqD8tKZEKvaK2", "19P6EYhu6kZzRy9Au4wRRZVE8RemrxPbZP",
-#                     "1bones8KbQge9euDn523z5wVhwkTP3uc1", "1Bow5EMqtDGV5n5xZVgdpRPJiiDK6XSjiC"]
-#         non_matching = ["2LgqButDNV2rVHe9DATt6WqD8tKZEKvaK2", "19Ry9Au4wRRZVE8RemrxPbZP",
-#                         "1bones8KbQge9euDn523z5wVhwkTP3uc12939", "1Bow5EMqtDGV5n5xZVgdpR"]
-#         for s in matching:
-#             self.assertEqual(self.parser.btc_addresses(s), [s])
-#         for s in non_matching:
-#             self.assertNotEqual(self.parser.btc_addresses(s), [s])
-#
-# class TestStreetAddresses(RegexTestCase):
-#
-#     def test_street_addresses(self):
-#         matching = ["checkout the new place at 101 main st.", "504 parkwood drive", "3 elm boulevard",
-#                     "500 elm street "]
-#         non_matching = ["101 main straight"]
-#
-#         for s in matching:
-#             self.assertTrue(self.parser.street_addresses(s))
-#         for s in non_matching:
-#             self.assertFalse(self.parser.street_addresses(s))
-#
-# class TestPoBoxes(RegexTestCase):
-#
-#     def test_po_boxes(self):
-#         matching = ["PO Box 123456",
-#                     "hey p.o. box 234234 hey"]
-#         non_matching = ["101 main straight"]
-#
-#
-#         for s in matching:
-#             self.assertTrue(self.parser.po_boxes(s))
-#         for s in non_matching:
-#             self.assertFalse(self.parser.po_boxes(s))
-#
-# class TestZipCodes(RegexTestCase):
-#
-#     def test_zip_codes(self):
-#         matching = ["02540", "02540-4119"]
-#         non_matching = ["101 main straight", "123456"]
-#
-#         for s in matching:
-#             self.assertTrue(self.parser.zip_codes(s))
-#         for s in non_matching:
-#             self.assertFalse(self.parser.zip_codes(s))
+
+@pytest.mark.parametrize('email', ["john.smith@gmail.com", "john_smith@gmail.com", "john@example.net"])
+def test_email(email):
+    assert parser().emails(email) == [email]
+
+
+@pytest.mark.parametrize('ip', ["127.0.0.1", "192.168.1.1", "8.8.8.8"])
+def test_ipv4(ip):
+    assert parser().ips(ip) == [ip]
+
+
+@pytest.mark.parametrize('ip', [
+    "fe80:0000:0000:0000:0204:61ff:fe9d:f156",
+    "fe80:0:0:0:204:61ff:fe9d:f156",
+    "fe80::204:61ff:fe9d:f156",
+    "fe80:0000:0000:0000:0204:61ff:254.157.241.86",
+    "fe80:0:0:0:0204:61ff:254.157.241.86",
+    "fe80::204:61ff:254.157.241.86", "::1"
+])
+def test_ipv6(ip):
+    assert parser().ipv6s(ip) == [ip]
+
+
+@pytest.mark.parametrize('price', ["$1.23", "$1", "$1,000", "$10,000.00"])
+def test_price(price):
+    assert parser().prices(price) == [price]
+
+
+@pytest.mark.parametrize('color', ["#fff", "#123", "#4e32ff", "#12345678"])
+def test_hex_color(color):
+    assert parser().hex_colors(color) == [color]
+
+
+@pytest.mark.parametrize('card_number', [
+    "0000-0000-0000-0000",
+    "0123456789012345",
+    "0000 0000 0000 0000",
+    "012345678901234"]
+)
+def test_credit_card(card_number):
+    assert parser().credit_cards(card_number) == [card_number]
+
+
+@pytest.mark.parametrize('btc', [
+    "1LgqButDNV2rVHe9DATt6WqD8tKZEKvaK2",
+    "19P6EYhu6kZzRy9Au4wRRZVE8RemrxPbZP",
+    "1bones8KbQge9euDn523z5wVhwkTP3uc1",
+    "1Bow5EMqtDGV5n5xZVgdpRPJiiDK6XSjiC"
+])
+def test_btc_addresses(btc):
+    assert parser().btc_addresses(btc) == [btc]
+
+
+@pytest.mark.parametrize('address', [
+    "checkout the new place at 101 main st.",
+    "504 parkwood drive",
+    "3 elm boulevard",
+    "500 elm street "
+])
+def test_street_addresses(address):
+    assert parser().street_addresses(address)
+
+
+@pytest.mark.parametrize('po_box', ["PO Box 123456", "hey p.o. box 234234 hey"])
+def test_po_boxes(po_box):
+    assert parser().po_boxes(po_box)
+
+
+@pytest.mark.parametrize('zip_code', ["02540", "02540-4119"])
+def test_zip_codes(zip_code):
+    assert parser().zip_codes(zip_code)
